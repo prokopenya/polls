85e9d3238e965a66c329aa5a38db955b120ca505
prokopenya <igorprokopenya@gmail.com>
2020-09-08 14:37:28 +0300

    init

---
+++ src/main/java/app/polls/model/Role.java
@@ -0,0 +1,42 @@
+package app.polls.model;
+
+import org.hibernate.annotations.NaturalId;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @NaturalId
+    @Column(length = 60)
+    private RoleName name;
+
+    public Role() {
+
+    }
+
+    public Role(RoleName name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RoleName getName() {
+        return name;
+    }
+
+    public void setName(RoleName name) {
+        this.name = name;
+    }
+}
